ad94604cbd1126e150042c4db351c6638f70a211
yhye1228 <41574178+yhye1228@users.noreply.github.com>
2025-08-29 16:23:20 +0900

    Create Dog.java

---
+++ Day2/OOP/OOPTest02/Dog.java
@@ -0,0 +1,39 @@
+public class Dog
+{
+    //1. 멤버변수
+    String name;
+    int age;
+    String color;
+    //2. 생성자 - 객체 초기화
+    Dog()
+    {
+        this.name = "댕댕이";
+        this.age = 3;
+        this.color = "누런색";
+    }
+    Dog(String name){
+        this.name = name;
+        //his.color = "누런색";
+    }
+    Dog(int age, String name, String color){
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+    //3. 멤버 메소드
+    String Run()
+    {
+        //System.out.println("달리다~~");
+        return "달리다~~";
+    }
+    String Eat()
+    {
+        //System.out.println("먹다.");
+        return "먹다. ";
+    }
+    String Bark()
+    {
+        //System.out.println("멍!멍!");
+        return "멍!멍!";
+    }
+}
