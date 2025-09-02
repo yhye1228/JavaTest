c3513d776490c24c8ea24e93d1c25ceb1c2aea0a
yhye1228 <41574178+yhye1228@users.noreply.github.com>
2025-09-02 13:58:42 +0900

    Create Car.java

---
+++ Day3/OOPTest06/Car.java
@@ -0,0 +1,16 @@
+package kr.co.verthasys.project1;
+
+//import com.google.Util;
+
+public class Car
+{
+    //1. 멤버 변수(필드)
+    public int speed;
+    //2. 생성자 (초기화 --> 변수의 값)
+    Car()
+    {
+        //this.speed = 100; //디폴트 : 0으로 초기화
+    }
+    //3. 멤버 메소드(생성자도 메소드), Getter/Setter 모두 메소드
+
+}
